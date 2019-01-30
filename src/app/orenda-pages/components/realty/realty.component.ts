808e7f02132dfce05bf68869794501706013e01a
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2019-01-30 15:45:54 +0200

    finish realty-page

--- src/app/orenda-pages/components/realty/realty.component.ts
+++ src/app/orenda-pages/components/realty/realty.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LangSevice } from 'src/app/_services/lang.service';
 
 @Component({
   selector: 'app-realty',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./realty.component.css']
 })
 export class RealtyComponent implements OnInit {
+  private lang = 'ukr';
+  private isUkr = true;
+  private orendaType = 'photo-realty';
 
-  constructor() { }
+  constructor(private langServ: LangSevice) { }
 
   ngOnInit() {
+    this.getLang();
   }
 
+  private getLang() {
+    this.langServ.obsLang.subscribe(
+      () => {
+        this.lang = this.langServ.obsLang.getValue();
+        this.isUkr = (this.lang === 'ukr');
+      }
+    );
+  }
 }

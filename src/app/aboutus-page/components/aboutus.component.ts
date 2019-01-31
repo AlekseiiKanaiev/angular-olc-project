bda8bf7e12908eb835e49d3617f5947a58019822
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2019-01-31 08:31:31 +0200

    starting aboutus-page

--- src/app/aboutus-page/components/aboutus.component.ts
+++ src/app/aboutus-page/components/aboutus.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LangSevice } from 'src/app/_services/lang.service';
+import store from 'src/app/_store/store';
+import { User } from 'src/app/_models/user.model';
 
 @Component({
   selector: 'app-aboutus',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutus.component.css']
 })
 export class AboutusComponent implements OnInit {
+  private lang = 'ukr';
+  private isUkr = true;
+  private users: User[];
 
-  constructor() { }
+  constructor(private langServ: LangSevice) { }
 
   ngOnInit() {
+    this.getLang();
+    this.getUsers();
   }
 
+  private getLang() {
+    this.langServ.obsLang.subscribe(
+      () => {
+        this.lang = this.langServ.obsLang.getValue();
+        this.isUkr = (this.lang === 'ukr');
+      }
+    );
+  }
+
+  private getUsers() {
+    this.users = store.getState().users;
+    store.subscribe(
+        () => {
+            this.users = store.getState().users;
+        }
+    );
+  }
 }
